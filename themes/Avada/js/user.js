bca7a3858a57907106adabcc1849eac4879e7965
hathbanger <hathbanger.ah@gmail.com>
2015-11-16 18:49:18 -0700

    form animate

--- themes/Avada/js/user.js
+++ themes/Avada/js/user.js
@@ -1,6 +1,28 @@
-jQuery(document).ready(function($) {
-  var acct = $('.my-account');
-  var child = jQuery('#snav .menu-item');
+(function ($, root, undefined) {
+	console.log('working')
+	$(function () {		
+	'use strict';
 
-  acct.insertBefore(child.eq(1));
-})
+		
+	// .hover(function(e){e.preventDefault();console.log('workingg')})
+
+		$('#rev_slider_14_1_wrapper > div.tp-rightarrow.tparrows.default.round').on('click', function (e) {
+		    e.preventDefault();
+		    alert("hello");
+		});
+
+		var formCol = $('.formHider');
+
+		formCol.height(0);
+
+		var buttonShow = $('.labelsButton');
+
+		buttonShow.click(openForm(formCol));
+
+		function openForm(arr){
+			arr.animate({"height":"200px"});
+		}
+
+	});
+	
+})(jQuery, this);
